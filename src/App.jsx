0ecda6cb4af12cd8306e64666d4378c46597af7d
Jonathon E <je20k3@gmail.com>
2024-02-09 18:48:47 +0000

    Add init files + Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+   return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav class="navbar">
+        <div class="container-fluid">
+          <div class="navbar--text d-flex">
+                      <a>Clicky Game</a>
+                      <li>Click an image to begin!</li>
+                     <li>Score / Top score</li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
